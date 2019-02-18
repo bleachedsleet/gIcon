48017986ce513bc688b2ca901bc3d334ec05c3b4
scratchfive <scratchfive@users.noreply.github.com>
2019-02-18 12:40:34 -0700

    Initial Commit
    Hello World! This is the first commit, taking baby steps toward making Google Drive actually look good on Windows

---
+++ gicon.bat
@@ -0,0 +1,68 @@
+::---------------------------------------------------------------------------------------::
+:: This script will modify the Windows Explorer sidebar to include an icon for Google    ::
+:: Drive. It does this with a little registry tweak and is completely reversible and non-::
+:: invasive. Run without any arguments to see the help prompt. This script is also fully ::
+:: scriptable in itself, so you can feel free to include it in other scripts if you'd    ::
+:: like.                                                                                 ::
+::---------------------------------------------------------------------------------------::
+
+
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	goto main
+) else (
+    echo Run this script as Admin and try again
+    exit /B
+)
+exit /B
+
+:main
+set parameters=%1
+set loc=%2
+if not defined parameters (
+	goto help
+)
+if "%parameters%" == "-i" (
+	call :importKeys
+	exit /B
+) else (
+	if "%parameters%" == "-r" (
+		call :removeKeys
+		exit /B
+	)
+)
+exit /B
+
+:help
+echo.
+echo Use -i to install icon 
+echo Use -r to remove icon 
+echo.
+echo Set install path as second argument. Leave blank for default location.
+echo.
+exit /B
+
+:importKeys
+if not defined loc (
+	echo Custom location not set...using default path
+	set loc="%userprofile%\Google Drive"
+) 
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79} /ve /t REG_SZ /d "Google Drive" /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\DefaultIcon /ve /t REG_EXPAND_SZ /d "C:\Program Files\Google\Drive\googledrivesync.exe,0" /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79} /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0x1 /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79} /v SortOrderIndex /t REG_DWORD /d 0x42 /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\InProcServer32 /ve /t REG_EXPAND_SZ /d %%systemroot%%\system32\shell32.dll /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\Instance /v CLSID /t REG_SZ /d {0E5AAE11-A475-4c5b-AB00-C66DE400274E} /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\Instance\InitPropertyBag /v Attributes /t REG_DWORD /d 0x11 /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\Instance\InitPropertyBag /v TargetFolderPath /t REG_EXPAND_SZ /d %loc%\ /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\ShellFolder /v FolderValueFlags /t REG_DWORD /d 0x28 /f
+reg add HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79}\ShellFolder /v Attributes /t REG_DWORD /d 0xF080004D /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{9499128F-5BF8-4F88-989C-B5FE5F058E79} /ve /t REG_SZ /d "Google Drive" /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {9499128F-5BF8-4F88-989C-B5FE5F058E79} /t REG_DWORD /d 0x1 /f
+exit /B
+
+:removeKeys
+reg delete HKCU\Software\Classes\CLSID\{9499128F-5BF8-4F88-989C-B5FE5F058E79} /va /f
+exit /B
---
+++ readme.md
@@ -0,0 +1,12 @@
+# gIcon
+This is a simple little script for Windows that adds ever so slightly deeper integration with the Explorer shell. I got tired of Google ignoring basic conventions of storage providers on Windows by ignoring the File Explorer sidebar and instead using Quick Access pinning. This script changes all that by adding a Google Drive entry on the sidebar instead, just like OneDrive and Dropbox do. 
+
+This not only looks pretty, but it also has the added benefit of browsing directory trees directly from the sidebar. 
+
+## Usage
+Run without parameters for help. This script has full support for custom Google Drive paths but will default to the same default path that Google uses which is in your USERPROFILE folder. 
+
+```gicon.bat [-i | -r] "CUSTOM_PATH"
+```
+
+This script is non invasive and works by making a few registry modifications. The icon used is the default Backup & Sync icon included in Google's client. This allows the icon to remain dynamically attached to sync status so you can get a quick overview of if your Drive is in sync or not. Everything the script does can be reversed safely.
